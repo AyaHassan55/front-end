808f657f2f43c34e905bbff75e3e344fa8279b23
AyaAly9 <ayaaly929@gmail.com>
2025-10-21 22:56:19 +0300

    files structer & routes

---
+++ src/Pages/Website/Home.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h3>Hello</h3>
+      </header>
+    </div>
+  );
+}
---
+++ src/Pages/Website/Login.js
@@ -0,0 +1,7 @@
+export default function LoginPage() {
+  return (
+    <div>
+      <h3>Login page</h3>
+    </div>
+  );
+}
