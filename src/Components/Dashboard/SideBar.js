6e6896919e4d2d722bdd621299a3f1ee6d11b659
AyaHassan55 <haya98166@gmail.com>
2025-11-08 16:48:17 +0200

    add font awsome & make navlink in sidebar

--- src/Components/Dashboard/SideBar.js
+++ src/Components/Dashboard/SideBar.js
@@ -1,4 +1,15 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './Bars.css';
+import { faUsers } from '@fortawesome/free-solid-svg-icons';
+import { NavLink } from 'react-router-dom';
 export default function SideBar() {
-  return <div className='side-bar'>SideBar</div>;
+  return (
+    <div className='side-bar pt-3'>
+      <NavLink to={"users"} className="d-flex align-items-center gap-2 side-bar-link">
+        <FontAwesomeIcon icon={faUsers} />
+        <p className='m-0'>Users</p>
+      </NavLink>
+
+    </div>
+  );
 }
