b5d51973c23172bf60d143dee80c12abf6d5cca4
AyaHassan55 <haya98166@gmail.com>
2025-11-08 22:10:18 +0200

    edit & delete user part1

--- src/Pages/Dashboard/Users.js
+++ src/Pages/Dashboard/Users.js
@@ -1,14 +1,17 @@
 import { useEffect, useState } from "react";
-import { baseUrl, USERS } from "../../Api/Api";
+import {  USERS } from "../../Api/Api";
 
-import Cookie from "cookie-universal";
+
 import { Table } from 'react-bootstrap';
 import { Axios } from "../../Api/Axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPenToSquare, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
 
 
 export default function Users() {
   const [users, setUsers] = useState([]);
-  const cookie = new Cookie();
+ 
   useEffect(() => {
     Axios
       .get(`/${USERS}`)
@@ -17,11 +20,25 @@ export default function Users() {
       .catch((err) => console.log(err));
   }, []);
 
-  const usersShow = users.map((user,key) => (<tr key={key}>
-    <td>{key+1}</td>
+ 
+  const usersShow = users.map((user, key) => (<tr key={key}>
+    <td>{key + 1}</td>
     <td>{user.name}</td>
     <td>{user.email}</td>
+    <td >
+      <div className="d-flex align-items-center justify-content-center gap-2">
+        <Link to ={`${user.id}`}>
+          <FontAwesomeIcon icon={faPenToSquare} />
+        </Link>
+        <FontAwesomeIcon onClick={handleDelete} color="#bb0f0f" icon={faTrash} />
+      </div>
+    </td>
   </tr>))
+
+   // delete User
+  function handleDelete(){
+    
+  }
   return (<div className="bg-white p-2 w-100 rounded-3">
     <h4>Users</h4>
     <Table striped bordered hover>
@@ -35,7 +52,7 @@ export default function Users() {
         </tr>
       </thead>
       <tbody>
-        {usersShow }
+        {usersShow}
 
       </tbody>
     </Table>
