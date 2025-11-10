140abc2655343fd47251d7b067c8d5809d4d1d8b
AyaHassan55 <haya98166@gmail.com>
2025-11-10 17:55:59 +0200

    get categories from api and maping it to dispalay in options inside form select

--- src/Pages/Dashboard/AddProduct.js
+++ src/Pages/Dashboard/AddProduct.js
@@ -1,7 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 import { Form } from "react-bootstrap";
 import { Axios } from "../../Api/Axios";
-import { CATEGORY, PRODUCT, USER } from "../../Api/Api";
+import { CATEGORIES, CATEGORY, PRODUCT, PRODUCTS, USER } from "../../Api/Api";
 import LoadingSubmit from "../../Components/Loading/Loading";
 
 
@@ -16,6 +16,7 @@ export default function AddProduct() {
         About:'',
     })
     const [image,setImage] = useState('');
+    const [categories,setCategories] = useState([]);
     const [loading, setLoading] = useState(false);
 
      // useRef-----------------------
@@ -24,62 +25,81 @@ export default function AddProduct() {
             focus.current.focus();
         },[])
     //  handel change -----------------------------
-    function handleChange(){}
+    function handleChange(e){
+        setForm({...form,[e.target.name]:e.target.value})
+    }
+    // get all categories
+    useEffect(()=>{
+        Axios.get(`/${CATEGORIES}`)
+        .then((data)=>setCategories(data.data)).catch((err)=>console.log(err))
+    },[])
 
 
     // handle form submit
-    // async function handleSubmit(e) {
-    //     setLoading(true);
-    //     e.preventDefault();
+    async function handleSubmit(e) {
+        // setLoading(true);
+        // e.preventDefault();
 
-    //     const form =new FormData();
-    //     form.append('category',category);
-    //     form.append('title',title);
-    //     form.append('description',description);
-    //     form.append('price',price);
-    //     form.append('discount',discount);
-    //     form.append('a',discount);
+        // const form =new FormData();
+        // form.append('category',category);
+        // form.append('title',title);
+        // form.append('description',description);
+        // form.append('price',price);
+        // form.append('discount',discount);
+        // form.append('a',discount);
 
       
-    //     try {
-    //         const res = await Axios.post(`${PRODUCT}/add`, form);
-    //         window.location.pathname = '/dashboard/product/'
-    //     } catch (err) {
-    //         setLoading(false);
-    //         console.log(err);
-    //     }
-    // }
+        // try {
+        //     const res = await Axios.post(`${PRODUCT}/add`, form);
+        //     window.location.pathname = '/dashboard/product/'
+        // } catch (err) {
+        //     setLoading(false);
+        //     console.log(err);
+        // }
+    }
+
+    // categories show
+    const categoriesShow =categories.map((item,key) =>(
+        <option key={key} value={item.id}>{item.title}</option>
+    ))
     return (
         <>
             {loading && <LoadingSubmit />}
 
             <Form className="bg-white w-100  p-3" 
-            // onSubmit={handleSubmit}
+            onSubmit={handleSubmit}
             >
-                
+                <Form.Group className="mb-3" controlId="category">
+                    <Form.Label style={{fontWeight:'bold'}}>Category</Form.Label>
+                    <Form.Select name="category" value={form.category}  
+                    onChange={handleChange}  placeholder="Category..."  ref={focus}>
+                        {categoriesShow}
+                    </Form.Select>
+                </Form.Group>
+                {/* ---------------------------------------- */}
                 <Form.Group className="mb-3" controlId="title">
                     <Form.Label style={{fontWeight:'bold'}}>Title</Form.Label>
-                    <Form.Control  value={form.title} required onChange={handleChange} type="text" placeholder="Title..."  ref={focus}/>
+                    <Form.Control name="title" value={form.title} required onChange={handleChange} type="text" placeholder="Title..."  />
                 </Form.Group>
                 {/* ---------------------------------------- */}
                  <Form.Group className="mb-3" controlId="description">
                     <Form.Label style={{fontWeight:'bold'}}>Description</Form.Label>
-                    <Form.Control  value={form.description} required onChange={handleChange} type="text" placeholder="Description..."  />
+                    <Form.Control name="description" value={form.description} required onChange={handleChange} type="text" placeholder="Description..."  />
                 </Form.Group>
                 {/* ---------------------------------------- */}
                  <Form.Group className="mb-3" controlId="price">
                     <Form.Label style={{fontWeight:'bold'}}>Price</Form.Label>
-                    <Form.Control  value={form.price} required onChange={handleChange} type="text" placeholder="Price..."  />
+                    <Form.Control name="price"  value={form.price} required onChange={handleChange} type="text" placeholder="Price..."  />
                 </Form.Group>
                 {/* ---------------------------------------- */}
                  <Form.Group className="mb-3" controlId="discount">
                     <Form.Label style={{fontWeight:'bold'}}>Discount</Form.Label>
-                    <Form.Control  value={form.discount} required onChange={handleChange} type="text" placeholder="Discount..."  />
+                    <Form.Control name="discount"  value={form.discount} required onChange={handleChange} type="text" placeholder="Discount..."  />
                 </Form.Group>
                 {/* ---------------------------------------- */}
                  <Form.Group className="mb-3" controlId="About">
                     <Form.Label style={{fontWeight:'bold'}}>About</Form.Label>
-                    <Form.Control  value={form.About} required onChange={handleChange} type="text" placeholder="About..."  />
+                    <Form.Control name="About" value={form.About} required onChange={handleChange} type="text" placeholder="About..."  />
                 </Form.Group>
                 {/* ---------------------------------------- */}
                 
