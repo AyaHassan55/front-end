ca5b15438a4e706046450541c5167e7c9b0d200c
AyaHassan55 <haya98166@gmail.com>
2025-11-18 17:15:05 +0200

    add skelton loading design in categories page

--- src/Pages/Website/Categories/Categories.js
+++ src/Pages/Website/Categories/Categories.js
@@ -2,34 +2,48 @@ import { useEffect, useState } from "react";
 import { Axios } from "../../../Api/Axios";
 import { CATEGORIES } from "../../../Api/Api";
 import { Container } from "react-bootstrap";
+import Skeleton from "react-loading-skeleton";
 
 
-export default function WebsiteCategories(){
+export default function WebsiteCategories() {
     const [categories, setCategories] = useState([]);
-    useEffect(()=>{
-        Axios.get(`${CATEGORIES}`).then((data)=> setCategories(data.data));
-    },[])
-    const showCategories=categories.map((item,key)=>(
+    const [loading, setLoading] = useState(true);
+    useEffect(() => {
+        Axios.get(`${CATEGORIES}`).then((data) => setCategories(data.data)).finally(() => setLoading(false));
+    }, [])
+    const showCategories = categories.map((item, key) => (
         <div className="col-lg-2 col-md-6 col-12 bg-transparent border-0">
             <div className="m-1 bg-white border d-flex align-items-center justify-content-start gap-3 rounded py-2h-100">
-                <img className="ms-3" width={'50px'} src= {item.image ? item.image : '../../../Assets/Icons/Profile.png' } alt="just an image"/>
+                <img className="ms-3" width={'50px'} src={item.image ? item.image : '../../../Assets/Icons/Profile.png'} alt="just an image" />
                 <p className="m-0">
-                    {item.title.length > 12? item.title.slice(1,12) + '...' : item.title}
+                    {item.title.length > 12 ? item.title.slice(0, 12) + '...' : item.title}
                 </p>
             </div>
         </div>
 
     ));
-    return(
+    return (
         <>
-       
-        <div className="bg-secondary py-5">
-           <Container>
-            <div className="d-flex align-items-center justify-content-center flex-wrap row-gap-2">
-                {showCategories}
+
+            <div className="bg-secondary py-5">
+                <Container>
+                    <div className="d-flex align-items-center justify-content-center flex-wrap row-gap-2">
+                        {loading ? (
+                            Array.from({ length: categories.length || 30 }).map((_, i) => (
+                                <div key={i} className="col-lg-2 col-md-6 col-12">
+                                    <div className="mx-1">
+                                        <Skeleton height="100px" />
+                                    </div>
+                                </div>
+                            ))
+                        ) :
+
+                            (showCategories
+
+                            )}
+                    </div>
+                </Container>
             </div>
-           </Container>
-        </div>
         </>
     );
 }
