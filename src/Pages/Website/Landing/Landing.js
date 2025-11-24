7e7d8c6d83e97ff0148aec983b78f6c3f4d65969
AyaHassan55 <haya98166@gmail.com>
2025-11-24 10:45:11 +0200

    landing design update

--- src/Pages/Website/Landing/Landing.js
+++ src/Pages/Website/Landing/Landing.js
@@ -5,18 +5,23 @@ import { Link } from "react-router-dom";
 export default function Landing() {
     return (
         <>
-            <div className="d-flex align-items-center justify-content-center flex-wrap hand">
-                <Container>
-                    <div className="col-lg-5 col-md-8 col-12 text-md-start text-center">
-                        <h1 className="display-2 fw-bold">Shampo Nice!</h1>
-                        <h5 className="fw-normal" style={{ color: 'grey' }}>Another Nice Thing Which is used by someone i don't know it's a random text</h5>
-                        <Link to="/shop" className="btn btn-primary py-3 px-4 fw-bold text-light">Shop Now</Link>
-                    </div>
+            <div className="d-flex align-items-center justify-content-center flex-wrap hand position-relative">
 
-                </Container>
+             {/* <Container > */}
+                <div className= "content col-lg-5 col-md-8 col-12 text-md-start text-center position-absolute">
+                    {/* <h1 className="display-2 fw-bold">Shampo Nice!</h1> */}
+                    <h1 className="fw-normal mb-4" style={{color:'#003e29',marginBottom:'10px'}}>Grap up to 50%  on selected Headphone </h1>
+                    <Link to="/shop" className="btn btn-primary py-2 px-3 text-light " style={{borderRadius:'30px',height:'40px',textAlign:'center'}}>Shop Now</Link>
+                </div>
+                <div className="landing-img position-absolute">
+                    <img src={require("../../../Assets/images/girll.png")} width={"400px"} />
+                </div>
+             {/* </Container> */}
+              
+
 
             </div>
-            
+
         </>
     );
 }
