17cbeae04206110e99db1902dc3ed1d650e20cd3
AyaHassan55 <haya98166@gmail.com>
2025-12-03 19:21:43 +0200

    update loading component in dashboard

--- src/Components/Loading/Loading.js
+++ src/Components/Loading/Loading.js
@@ -1,7 +1,14 @@
 export default function LoadingSubmit() {
   return (
-    <div className="loading-overlay">
-      <div className="loader"></div>
+    <div class="loading-overlay">
+    <div class="spinner">
+      <div class="bounce1"></div>
+      <div class="bounce2"></div>
+      <div class="bounce3"></div>
     </div>
+    </div>
+    // <div className="loading-overlay">
+    //   <div className="loader"></div>
+    // </div>
   );
 }
